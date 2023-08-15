932e690425ab36784fbb0cb44c675acb1a6183cf
muhammadhussainharoon <hussain356q@gmail.com>
2023-08-15 12:55:29 +0500

    assignment done

---
+++ app.js
@@ -0,0 +1,76 @@
+const num1 = 10;
+const num2 = 20;
+console.log(num1 + num2);
+
+
+
+
+// function sum(num3, num4) {
+//     return num3 + num4
+// }
+// const num3 = +prompt("enter your fist number")
+// const num4 = +prompt("enter your last number")
+// console.log(num3 + num4);
+
+
+
+
+function salam(m, n) {
+console.log("bhai ap ka name" + " " + m + n + " " + "hai");
+}
+salam("muhammad", "hussain")
+
+
+
+
+
+const num5 = +prompt("enter your number")
+const num6 = +prompt("enter your number")
+const operator = prompt("enter your operator")
+switch (operator) {
+    case ("+"):
+        const sum = num5 + num6;
+        console.log(sum);
+        break
+    case ("-"):
+        const sub = num5 - num6;
+        console.log(sub);
+        break
+    case ("*"):
+        const mul = num5 * num6;
+        console.log(mul);
+        break
+    case ("/"):
+        const div = num5 / num6;
+        console.log(div);
+        break
+    case ("%"):
+        const mod = num5 % num6;
+        console.log(mod);
+        break
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
